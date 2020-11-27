730cbfbdf44f8eedb505556a169357839feb56d3
mnur <khsdo12@gmail.com>
2020-11-26 20:08:57 -0800

    Fix apt source error

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 ## Change apt src
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cp ./sources.list /etc/apt/sources.list
+#cp ./sources.list /etc/apt/sources.list
+sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/us.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/us.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 apt-get update
 
 ## Add arch
