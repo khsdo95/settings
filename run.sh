6a8edeff1f4840c7bc97d347b66793dac57fe115
mnur <khsdo12@gmail.com>
2020-11-26 20:11:29 -0800

    Remove sudo command in script

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 ## Change apt src
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 #cp ./sources.list /etc/apt/sources.list
-sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo sed -i 's/us.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-sudo sed -i 's/us.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sed -i 's/us.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sed -i 's/us.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 apt-get update
 
 ## Add arch
